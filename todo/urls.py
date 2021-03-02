df3934ec6fc0cc5901f0e7f6bcdca61062d88b57
Vladislav Naumov <qq.vladis@gmail.com>
2021-03-02 18:00:06 +0300

    reloaded  project

---
+++ todo/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import (
+    TodolistView,
+    TodoDeletView,
+    TodoCreateView,
+    TodoUpdateView
+
+)
+
+
+urlpatterns = [
+    path('', TodolistView.as_view(), name='todo'),
+    path('<int:pk>/delete/', TodoDeletView.as_view(), name='delete_todo'),
+    path('<int:pk>/create/', TodoCreateView.as_view(), name='create_todo'),
+    path('<int:pk>/update/', TodoUpdateView.as_view(), name='update_todo'),
+    ]
---
+++ todo/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from .models import TodoItem
+from django.views.generic import ListView
+from django.views.generic.edit import DeleteView, UpdateView, CreateView
+from django.urls import reverse_lazy
+
+
+# Create your views here.
+
+class TodolistView(ListView):
+    model = TodoItem
+    template_name = 'todo.html'
+
+class TodoDeletView(DeleteView):
+    model = TodoItem
+    template_name = 'delete_todo.html'
+    success_url = reverse_lazy('todo')
+
+class TodoCreateView(CreateView):
+    model = TodoItem
+    template_name = 'create_todo.html'
+    fields = ('title', 'body')
+    success_url = reverse_lazy('todo')
+
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class TodoUpdateView(UpdateView):
+    model = TodoItem
+    fields = ('title', 'body')
+    template_name = 'update_todo.html'
+    success_url = reverse_lazy('todo')
